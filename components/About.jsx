94cc5bfb826ed6ace881db103c6e346abb0519b4
ykyadmas <yekoyeadmas@gmail.com>
2023-10-29 04:31:06 +0300

    commit

--- components/About.jsx
+++ components/About.jsx
@@ -7,9 +7,9 @@ const About =()=>{
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src={"/net.jpg" }
+        <Image src="/net.jpg" 
         width={500} height={500} 
-        className={'h-[400px]' }
+        className='h-[400px]' 
         alt={'fullstack banner'} />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
